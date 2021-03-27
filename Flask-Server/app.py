986206126298b435f4125a03a255f86ed8fe46bb
sendmithunb@gmail.com <sendmithunb@gmail.com>
2021-03-27 13:51:55 -0500

    integrate camera and flask

--- Flask-Server/app.py
+++ Flask-Server/app.py
@@ -1,20 +1,9 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, url_for 
 import cv2
-
+from camera import main
 app = Flask(__name__)
 camera_status = "On"
 webcam = cv2.VideoCapture(0)
-
-def gen_frames():  
-    while True:
-        success, frame = webcam.read() 
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
 
 @app.route('/')
 def home():
@@ -24,15 +13,13 @@ def home():
 def toggle():
     if (camera_status == "On"):
         camera_status = "Off"
+        main(False)
     else:
         camera_status = "On"
+        main(True)
 
     # Add trigger here          
-    return render_template('index.html', status=camera_status)
-
-@app.route('/preview') 
-def preview():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return render_template(url_for('index'), status=camera_status)
 
 if __name__ == '__main__':
     app.run()
---
+++ Flask-Server/camera.py
@@ -0,0 +1,59 @@
+import pyvirtualcam
+import numpy as np
+import cv2
+from PIL import Image
+import os
+
+video = cv2.VideoCapture(0)
+
+inputShape = video.read()[1].shape
+camHeight = inputShape[0]
+camWidth = inputShape[1]
+
+
+def getEmotions(frame):
+    #ML results
+    return "sad"
+
+def writeEmotions(result):
+    
+    script_dir = os.path.dirname(__file__)
+    rel_path = f"assets/{result}.png"
+    output = os.path.join(script_dir, rel_path)
+    return output
+
+def alpha_to_color(image, color=(255, 255, 255)):
+    x = np.array(image)
+    r, g, b, a = np.rollaxis(x, axis=-1)
+    r[a == 0] = color[0]
+    g[a == 0] = color[1]
+    b[a == 0] = color[2] 
+    x = np.dstack([r, g, b, a])
+    return Image.fromarray(x, 'RGBA')
+
+
+def main(toggle):
+    with pyvirtualcam.Camera(height=camHeight, width=camWidth, fps=30, backend = 'obs') as cam:
+        print(f'Using virtual camera: {cam.device}')
+        while toggle == True:
+            ret, frame = video.read()
+            if not ret:
+                raise RuntimeError('Error fetching frame')
+            
+            RGBframe = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            emojiFrame = Image.new('RGB', (camWidth, camHeight), color = 'black')
+            
+            emojiOverlay = alpha_to_color(Image.open(writeEmotions(getEmotions(RGBframe))))
+            width, height= emojiOverlay.size
+    
+            offset = ((camWidth - int(width )) // 2, (camHeight - int(height)) // 2)
+            emojiFrame.paste(emojiOverlay, offset)
+            emojiFrame = np.array(emojiFrame)
+            
+            cam.send(emojiFrame)
+            cam.sleep_until_next_frame()
+
+if __name__ == "__main__":
+    main(True)
+
