a1694fd245b6d6b718eade90ab9f786397ec0987
Kaustubh Sonawane <sona.om78@gmail.com>
2021-03-27 14:09:55 -0500

    fix server

--- Flask-Server/app.py
+++ Flask-Server/app.py
@@ -1,25 +1,68 @@
 from flask import Flask, render_template, Response, url_for 
 import cv2
-from camera import main
+import pyvirtualcam
+import numpy as np
+import cv2
+from PIL import Image
+import os
+
 app = Flask(__name__)
 camera_status = "On"
 webcam = cv2.VideoCapture(0)
 
 @app.route('/')
-def home():
+def index():
+    camera(True)
     return render_template("index.html", status=camera_status)
 
-@app.route('/toggle')
-def toggle():
-    if (camera_status == "On"):
-        camera_status = "Off"
-        main(False)
-    else:
-        camera_status = "On"
-        main(True)
+def getEmotions(frame):
+    #ML results
+    return "sad"
 
-    # Add trigger here          
-    return render_template(url_for('index'), status=camera_status)
+def writeEmotions(result):
+    
+    script_dir = os.path.dirname(__file__)
+    rel_path = f"assets/{result}.png"
+    output = os.path.join(script_dir, rel_path)
+    return output
+
+def alpha_to_color(image, color=(255, 255, 255)):
+    x = np.array(image)
+    r, g, b, a = np.rollaxis(x, axis=-1)
+    r[a == 0] = color[0]
+    g[a == 0] = color[1]
+    b[a == 0] = color[2] 
+    x = np.dstack([r, g, b, a])
+    return Image.fromarray(x, 'RGBA')
+
+
+def camera(toggle):
+    video = cv2.VideoCapture(0)
+    inputShape = video.read()[1].shape
+    camHeight = inputShape[0]
+    camWidth = inputShape[1]
+
+    with pyvirtualcam.Camera(height=camHeight, width=camWidth, fps=30, backend = 'obs') as cam:
+        print(f'Using virtual camera: {cam.device}')
+        while toggle == True:
+            ret, frame = video.read()
+            if not ret:
+                raise RuntimeError('Error fetching frame')
+            
+            RGBframe = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            emojiFrame = Image.new('RGB', (camWidth, camHeight), color = 'black')
+            
+            emojiOverlay = alpha_to_color(Image.open(writeEmotions(getEmotions(RGBframe))))
+            width, height= emojiOverlay.size
+    
+            offset = ((camWidth - int(width )) // 2, (camHeight - int(height)) // 2)
+            emojiFrame.paste(emojiOverlay, offset)
+            emojiFrame = np.array(emojiFrame)
+            
+            cam.send(emojiFrame)
+            cam.sleep_until_next_frame()
+
 
 if __name__ == '__main__':
     app.run()
