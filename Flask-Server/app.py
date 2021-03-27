7e8147ad8c066bff3d920fb0e8cc82b1e7b7498f
sendmithunb@gmail.com <sendmithunb@gmail.com>
2021-03-27 13:42:31 -0500

    update flask ui

--- Flask-Server/app.py
+++ Flask-Server/app.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template
-from cv2 import VideoCapture
+from flask import Flask, render_template, Response
+import cv2
 
 app = Flask(__name__)
 camera_status = "On"
+webcam = cv2.VideoCapture(0)
+
+def gen_frames():  
+    while True:
+        success, frame = webcam.read() 
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
 
 @app.route('/')
 def home():
@@ -15,8 +27,12 @@ def toggle():
     else:
         camera_status = "On"
 
-    # Add trigger here
+    # Add trigger here          
     return render_template('index.html', status=camera_status)
 
+@app.route('/preview') 
+def preview():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
