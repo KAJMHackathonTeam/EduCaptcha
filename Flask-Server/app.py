591c0c201601941ba64d59ed2ff7d974ede0fed9
sendmithunb@gmail.com <sendmithunb@gmail.com>
2021-03-27 09:47:41 -0500

    update flask ui

--- Flask-Server/app.py
+++ Flask-Server/app.py
@@ -5,11 +5,18 @@ camera_status = "On"
 
 @app.route('/')
 def home():
-    return render_template("index.html", status=camera_status);
+    return render_template("index.html", status=camera_status)
 
-@app.route('/on')
-def on():
-    if ()
+@app.route('/toggle')
+def toggle():
+    if (camera_status == "On"):
+        camera_status = "Off"
+    else:
+        camera_status = "On"
+
+    # Add trigger here
+
     return render_template('index.html', status=camera_status)
+
 if __name__ == '__main__':
     app.run(debug=True)
