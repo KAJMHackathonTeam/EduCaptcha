91add1dca66f14490adc5a4c3c7b03641bfb139f
Kaustubh Sonawane <sona.om78@gmail.com>
2021-03-26 22:21:48 -0500

    fix

--- Virtual-Camera/camera.py
+++ Virtual-Camera/camera.py
@@ -1,10 +1,25 @@
 import pyvirtualcam
 import numpy as np
+import cv2
+from PIL import Image
 
-with pyvirtualcam.Camera(width=1280, height=720, fps=20) as cam:
+video = cv2.VideoCapture(0)
+
+inputShape = video.read()[1].shape
+camHeight = inputShape[0]
+camWidth = inputShape[1]
+
+with pyvirtualcam.Camera(height=camHeight, width=camWidth, fps=30, backend = 'obs') as cam:
     print(f'Using virtual camera: {cam.device}')
     frame = np.zeros((cam.height, cam.width, 3), np.uint8)  # RGB
     while True:
-        frame[:] = cam.frames_sent % 255  # grayscale animation
-        cam.send(frame)
+        active, frame = video.read()
+        if not active:
+            raise RuntimeError('Error fetching frame')
+
+        RGBframe = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        #Backend
+
+        cam.send(RGBframe)
         cam.sleep_until_next_frame()
