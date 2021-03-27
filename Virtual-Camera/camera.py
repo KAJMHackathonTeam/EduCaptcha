53e213ff241b6c0b9ef5ef7574669bb6a96222ba
Kaustubh Sonawane <sona.om78@gmail.com>
2021-03-27 13:42:12 -0500

    fix overlay add

--- Virtual-Camera/camera.py
+++ Virtual-Camera/camera.py
@@ -2,7 +2,6 @@ import pyvirtualcam
 import numpy as np
 import cv2
 from PIL import Image
-from urllib.request import urlopen
 import os
 
 video = cv2.VideoCapture(0)
@@ -12,9 +11,9 @@ camHeight = inputShape[0]
 camWidth = inputShape[1]
 
 
-def getEmotions():
+def getEmotions(frame):
     #ML results
-    return()
+    return "sad"
 
 def writeEmotions(result):
     
@@ -32,25 +31,29 @@ def alpha_to_color(image, color=(255, 255, 255)):
     x = np.dstack([r, g, b, a])
     return Image.fromarray(x, 'RGBA')
 
-with pyvirtualcam.Camera(height=camHeight, width=camWidth, fps=30, backend = 'obs') as cam:
-    print(f'Using virtual camera: {cam.device}')
-    while True:
-        ret, frame = video.read()
-        if not ret:
-            raise RuntimeError('Error fetching frame')
 
-        RGBframe = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+def main(toggle):
+    with pyvirtualcam.Camera(height=camHeight, width=camWidth, fps=30, backend = 'obs') as cam:
+        print(f'Using virtual camera: {cam.device}')
+        while toggle == True:
+            ret, frame = video.read()
+            if not ret:
+                raise RuntimeError('Error fetching frame')
+            
+            RGBframe = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        emojiFrame = Image.new('RGB', (camWidth, camHeight), color = 'black')
-        
-        emojiOverlay = alpha_to_color(Image.open(writeEmotions(getEmotions(RGBframe))))
-        width, height= emojiOverlay.size
-   
-        offset = ((camWidth - int(width )) // 2, (camHeight - int(height)) // 2)
-        emojiFrame.paste(emojiOverlay, offset)
-        emojiFrame = np.array(emojiFrame)
-        
-        cam.send(emojiFrame)
-        cam.sleep_until_next_frame()
+            emojiFrame = Image.new('RGB', (camWidth, camHeight), color = 'black')
+            
+            emojiOverlay = alpha_to_color(Image.open(writeEmotions(getEmotions(RGBframe))))
+            width, height= emojiOverlay.size
+    
+            offset = ((camWidth - int(width )) // 2, (camHeight - int(height)) // 2)
+            emojiFrame.paste(emojiOverlay, offset)
+            emojiFrame = np.array(emojiFrame)
+            
+            cam.send(emojiFrame)
+            cam.sleep_until_next_frame()
 
+if __name__ == "__main__":
+    main(True)
 
